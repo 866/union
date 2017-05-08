aad197e53ecf6860f8106c6fa787b4dd8be90905
Viktor Paslai <viktor.paslai@globallogic.com>
2017-05-08 11:03:17 +0300

    Move initDB to main

--- main.go
+++ main.go
@@ -3,9 +3,43 @@ package main
 import (
 	_ "union/routers"
 	"github.com/astaxie/beego"
+	"union/db"
+	"union/messages"
+	"github.com/satori/go.uuid"
+	"encoding/json"
 )
 
+
+// initialize lmdb database
+func initLMDB() {
+	lmdb, err := db.MakeLMDBHandler("/tmp", "mydb")
+	if err != nil {
+		panic(err)
+	}
+	// Add random proposal to the database
+	id := uuid.NewV4()
+	beego.Info("Prop ID: ", id.String())
+	prop := messages.Proposal{}
+	prop.FillRandom()
+	data, _ := json.Marshal(prop)
+	lmdb.Write(id.Bytes(), data)
+	// Add random chat message to the database
+	id = uuid.NewV4()
+	beego.Info("Chat Message ID: ", id.String())
+	chat := messages.ChatMessage{}
+	chat.FillRandom()
+	data, _ = json.Marshal(chat)
+	lmdb.Write(id.Bytes(), data)
+	// Global database
+	db.DB = lmdb
+}
+
+
 func main() {
+	// init the database
+	initLMDB()
+	beego.Info("DB is initialized.")
+
 	beego.Run()
 }
 
--- routers/router.go
+++ routers/router.go
@@ -3,32 +3,9 @@ package routers
 import (
 	"union/controllers"
 	"github.com/astaxie/beego"
-	"union/db"
-	"github.com/satori/go.uuid"
-	"fmt"
-	"union/messages"
-	"encoding/json"
 )
 
-// initialize lmdb database
-func initLMDB() {
-	lmdb, err := db.MakeLMDBHandler("/tmp", "mydb")
-	if err != nil {
-		panic(err)
-	}
-	id := uuid.NewV4()
-	fmt.Println("Prop ID: ", id.String())
-	prop := messages.Proposal{}
-	prop.FillRandom()
-	data, _ := json.Marshal(prop)
-	lmdb.Write(id.Bytes(), data)
-	db.DB = lmdb
-}
-
 func init() {
-	// init the database
-	initLMDB()
-
 	beego.Router("/", &controllers.MainController{})
 	// Info controllers
 	beego.Router("/proposal", &controllers.ProposalController{})
