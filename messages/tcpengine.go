cc524c965b29d9da5ba90c87aa7d6495130e89fa
Victor Paslay <comrazvictor@gmail.com>
2017-05-15 20:10:26 +0100

    Add tcpwengine

--- messages/tcpengine.go
+++ messages/tcpengine.go
@@ -1,10 +1,33 @@
-// tcpengine.go implements tcp client that communicates with trigger and provides gameplay
+// tcpengine.go implements tcp client that communicates with trigger, websockets and provides gameplay
 // 866
 // All Rights Reserved
 
 package messages
 
-// TCPEngine is a gameplay engine that works with TCP trigger server.
-type TCPEngine struct {
+import (
+	"github.com/gorilla/websocket"
+	"net"
+	"union/db"
+)
 
+// TCPWSEngine is a gameplay engine that works with TCP trigger server.
+// It provides chatting ability, interaction with the database, websocket connections handling.
+type TCPWSEngine struct {
+	ws      []*websocket.Conn
+	trigger net.Conn
+	db      db.DBHandler
+}
+
+// Close finishes all open objects.
+func (e *TCPWSEngine) Close() {
+	for _, conn := range e.ws {
+		conn.Close()
+	}
+	e.db.Close()
+	e.trigger.Close()
+}
+
+// internalLoop accepts new websocket connections and handles basic operations with them
+func (e *TCPWSEngine) internalLoop() {
+	// TODO
 }
